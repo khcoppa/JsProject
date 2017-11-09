5115a27a5d635ecfa823f4bad463078cc4602881
Kevin Coppa <khc339@nyu.edu>
2017-11-09 17:43:49 -0500

    Create Object files

---
+++ Objects/Dot.js
@@ -0,0 +1,42 @@
+export default class Dot {
+  constructor(props) {
+    this.color = props.color;
+    this.posX = props.pos.x;
+    this.posY = props.pos.y;
+    // this.radius = 50;
+    // this.selected = false;
+  }
+
+  draw() {
+
+  }
+
+  collapse() {
+
+  }
+
+  glow() {
+
+  }
+
+  drop() {
+
+  }
+
+  shake() {
+
+  }
+
+  hovered() {
+    // check mouse position
+
+  }
+
+  isNextTo() {
+    // must be above, below, left, right
+  }
+
+  connectible() {
+    // must be next to and same color
+  }
+}
---
+++ Objects/Grid.js
@@ -0,0 +1,17 @@
+import Dot from './Dot.js';
+
+class Grid {
+  constructor() {
+    this.makeGrid();
+  }
+
+  makeGrid() {
+    this.grid = [];
+    for (let yPos = 0; yPos < 7; yPos++) {
+      let dotRow = [];
+      for (let xPos = 0; xPos < 7, yPos++) {
+
+      }
+    }
+  }
+}
