cbae18568aaecfd5535fdbe9484aacf5e4f9c7ef
Kevin Coppa <khc339@nyu.edu>
2017-11-11 11:31:34 -0500

    Add canLinkTo function on Dot class

--- js/Dot.js
+++ js/Dot.js
@@ -1,60 +1,63 @@
 class Dot {
   constructor(props) {
-    this.posX = props.posX;
-    this.posY = props.posY;
+    this.pos = props.pos
     this.radius = 17;
-    this.color = undefined;
-    this.canvasPos = {};
-    // this.radius = 50;
-    // this.selected = false;
-    this.setRandomColor();
+    this.color = props.color;
+    this.setStill();
   }
 
-  setRandomColor() {
-    this.color = 'purple'
+  setStill() {
+    this.isAnchor = false;
   }
 
-  setPrimaryColor() {
-
+  setAnchor() {
+    this.isAnchor = true;
   }
 
-  drawDot(ctx, area) {
-    this.canvasPos = {
-      canvasX: this.posX * area + (area / 2),
-      canvasY: this.posY * area + (area / 2)
+  drawDot(ctx, area, mousePos) {
+    // cite code for making dots in grid on canvas
+    this.gridPos = {
+      x: this.pos.row * area + (area / 2),
+      y: this.pos.col * area + (area / 2)
     }
+    //
     ctx.beginPath();
-    ctx.arc(this.canvasPos.canvasX, this.canvasPos.canvasY, this.radius, 0, Math.PI*2);
+    ctx.arc(this.gridPos.x, this.gridPos.y, this.radius, 0, Math.PI*2);
     ctx.fillStyle = this.color;
     ctx.fill();
   }
 
-  collapse() {
-
+  isSelected(mousePos) {
+    // cite formula for finding mousePos on top of circle
+    const x = mousePos.x - this.gridPos.x;
+    const y = mousePos.y - this.gridPos.y;
+    return (x**2 + y**2) <= (this.radius**2)
+    //
   }
 
-  glow() {
-
+  canLinkTo(dot) {
+    const checkColor = dot.color === this.color;
+    return (checkColor && this.isNextTo(dot.pos))
   }
 
-  drop() {
-
+  isNextTo (dotPos) {
+    // check top
+    if (this.pos.x === dotPos.x && (this.pos.y + 1) === dotPos.y) {
+      return true;
+    }
+    // check bottom
+    else if (this.pos.x === dotPos.x && (this.pos.y - 1) === dotPos.y) {
+      return true;
+    }
+    // check right
+    else if ((this.pos.x + 1) === dotPos.x && this.pos.y === dotPos.y) {
+      return true;
+    }
+    // check left
+    else if ((this.pos.x - 1) === dotPos.x && this.pos.y === dotPos.y) {
+      return true;
+    }
+    return false;
   }
 
-  shake() {
-
-  }
-
-  hovered() {
-    // check mouse position
-
-  }
-
-  isNextTo() {
-    // must be above, below, left, right
-  }
-
-  connectible() {
-    // must be next to and same color
-  }
 }
